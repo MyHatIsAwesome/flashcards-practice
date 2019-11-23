9af27e0c4f56c82860e8873e5a78a75492c23ba3
Joseph Westwood <joe@indiespring.com>
2019-11-23 12:09:06 +0000

    Added root route. CaCan start app using 'nodemon' command

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('I Love Treehouse');
+});
+
 app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
