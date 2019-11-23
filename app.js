4f6ed5d688c3ce31fc3678362c823fb6105016f7
Joseph Westwood <joe@indiespring.com>
2019-11-23 13:23:40 +0000

    Added hello-goodbye cookie setting routes

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const PORT = 3000;
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    
+    if (name) {
+        res.render('index', { name });
+    } else {
+        res.redirect('/hello');
+    }
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
 });
 
 app.get('/cards', (req, res) => {
-    res.render('cards', {
+    res.render('card', {
         prompt: "Who is buried in Grant's Tomb?",
         hint: "Think about who's tomb it is."
     });
